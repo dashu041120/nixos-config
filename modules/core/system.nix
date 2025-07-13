8496f693442b6ea98ee836ea5e1955a5d0803d77
dashu041120 <zhangjingduan@msn.com>
2025-07-13 15:11:56 +0800

    - fix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -12,7 +12,7 @@
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
-        "https://ghostty.cachix.org"
+        # "https://ghostty.cachix.org"
         # cache mirror located in China
         # status: https://mirror.sjtu.edu.cn/
         # "https://mirror.sjtu.edu.cn/nix-channels/store"
@@ -25,7 +25,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+        # "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
       builders-use-substitutes = true;
     };
