b4d02167b77ae8405f6226005ef02942849d331b
dashu041120 <zhangjingduan@msn.com>
2025-11-08 15:06:11 +0800

    fix something

--- home/dashu/default.nix
+++ home/dashu/default.nix
@@ -7,33 +7,71 @@
 
   imports = [
     ./home.nix
-    ./alacritty.nix
-    ./bat.nix
-    ./browser.nix
+#     # TODO
+    # ./aseprite.nix           # pixel art editor
+    ./alacritty.nix         # terminal emulator
+#     ./audacious.nix                   # music player
+    ./bat.nix                         # better cat command
+    ./browser.nix                     # firefox based browser
+    # ./btop.nix                        # resouces monitor 
+#     ./cava.nix                        # audio visualizer
+#     ./discord.nix                     # discord
     ./dconf.nix
-    ./fzf.nix
-    ./gaming.nix
-    ./ghostty.nix
-    ./fcitx5
-    ./fonts.nix
+#     ./fastfetch.nix                   # fetch tool
+#     ./flow.nix                        # terminal text editor
+    ./fzf.nix                         # fuzzy finder
+    ./gaming.nix                      # packages related to gaming
+    ./ghostty.nix                     # terminal
+    # ./gpu-optimization.nix            # GPU performance optimization
+#     ./git.nix                         # version control
+#     ./gnome.nix                       # gnome apps
+
+    # ./hyprland
+
+    ./fcitx5                     # window manager
+    ./fonts.nix                   # font configuration
     ./cli.nix
     ./dev.nix
     ./gui.nix
+    # ./looking-glass/kvmfr.nix
     ./vscode.nix
     ./xdg.nix
-    ./yazi.nix
-    ./nix-direnv.nix
-    ./niri
+    ./yazi.nix                        # file manager
+#     ./kitty.nix                       # terminal
+#     ./lazygit.nix
+#     ./nemo.nix                        # file manager
+    ./nix-direnv.nix                 # nix-direnv integration
+#     ./nvim.nix                        # neovim editor
+    ./niri                        # niri
     ./oh-my-posh.nix
-    ./obs-studio.nix
+    ./obs-studio.nix                 # screen recorder
+#     ./p10k/p10k.nix
+#     ./packages                        # other packages
     ./pro-audio
-    ./starship.nix
-    # 如需启用，取消注释
-    # ./hyprland
+#     ./retroarch.nix  
+#     ./rofi.nix                        # launcher
+#     ./scripts/scripts.nix             # personal scripts
+#     ./ssh.nix                         # ssh config
+    ./starship.nix                     # shell prompt
+#     ./superfile/superfile.nix         # terminal file manager
+#     ./swaylock.nix                    # lock screen
+#     ./swayosd.nix                     # brightness / volume wiget
+#     ./swaync/swaync.nix               # notification deamon
+      ./social.nix                     # social apps
+      ./theme
+#     # ./viewnior.nix                    # image viewer
+
+    # ./variables.nix
+
+#     ./waybar                          # status bar
+#     ./waypaper.nix                    # GUI wallpaper picker
+    ./wps-office.nix                # WPS office
+#     ./xdg-mimes.nix                   # xdg config
+    ./zsh.nix                            # shell
   ];
 
-  programs.git = {
-    userName = "dashu041120";
-    userEmail = "zhangjingduan@msn.com";
+  programs.git.settings = {
+    user.name = "dashu041120";
+    user.email = "zhangjingduan@msn.com";
   };
 }
