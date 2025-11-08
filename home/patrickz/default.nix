06ac1405d3e8befb75b2bd515c3fb2dcf519aaaf
dashu041120 <zhangjingduan@msn.com>
2025-11-08 14:39:00 +0800

    fix: correct patrickz configuration imports
    - Remove non-existent module imports (alacritty, bat, browser, etc.)
    - Only include modules that exist in patrickz directory (home.nix)
    - patrickz@desktop configuration now evaluates correctly
    - Both homeConfigurations can be properly evaluated

--- home/patrickz/default.nix
+++ home/patrickz/default.nix
@@ -7,20 +7,6 @@
 
   imports = [
     ./home.nix
-    ./alacritty.nix
-    ./bat.nix
-    ./browser.nix
-    ./dconf.nix
-    ./fzf.nix
-    ./fonts.nix
-    ./cli.nix
-    ./dev.nix
-    ./gui.nix
-    ./vscode.nix
-    ./xdg.nix
-    ./yazi.nix
-    ./nix-direnv.nix
-    ./starship.nix
   ];
 
   programs.git = {
